4e1b4fe1e1642d38edd30ff0ddc01b113dcc66fb
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2022-07-03 23:14:05 +0800

    update

--- for-newbies/for-newbies-js.js
+++ for-newbies/for-newbies-js.js
@@ -6,14 +6,13 @@ var in_q=a_chosen=document.querySelector('.in-question');
 var ning_q;
 var score=0;
 
-
 //為題目上編號
 for(i=0;i<sq.length;i++){
     classname="QNo"+i.toString();
     sq[i].classList.add(classname);
 }
 
-//選定所有第一個答案
+//選定所有的偽答案(第六個)
 function choose_6(){
     ans_No1=document.querySelectorAll('.single-question .ans-item:nth-child(6)');
     for(i=0;i<ans_No1.length;i++){
@@ -21,6 +20,7 @@ function choose_6(){
         ans_No1[i].classList.add(classname);
     }
 }
+//為答案上編號
 function numing_ans(ansno){
     ansno_modified=ansno+1;
     ans_of_num=document.querySelectorAll(`.single-question .ans-item:nth-child(${ansno_modified.toString()})`);
@@ -30,7 +30,6 @@ function numing_ans(ansno){
     }
 }
 
-
 choose_6();
 numing_ans(0);
 numing_ans(1);
@@ -38,6 +37,26 @@ numing_ans(2);
 numing_ans(3);
 numing_ans(4);
 numing_ans(5);
+
+document.querySelector('.QNo0>.q-pr').innerHTML=Q0content;
+document.querySelector('.QNo1>.q-pr').innerHTML=Q1content;
+
+
+document.querySelector('.QNo0 .AnsNo0 .ans-pr').innerHTML=Q0Ans0;
+document.querySelector('.QNo0 .AnsNo1 .ans-pr').innerHTML=Q0Ans1;
+document.querySelector('.QNo0 .AnsNo2 .ans-pr').innerHTML=Q0Ans2;
+document.querySelector('.QNo0 .AnsNo3 .ans-pr').innerHTML=Q0Ans3;
+document.querySelector('.QNo0 .AnsNo4 .ans-pr').innerHTML=Q0Ans4;
+document.querySelector('.QNo1 .AnsNo0 .ans-pr').innerHTML=Q1Ans0;
+document.querySelector('.QNo1 .AnsNo1 .ans-pr').innerHTML=Q1Ans1;
+document.querySelector('.QNo1 .AnsNo2 .ans-pr').innerHTML=Q1Ans2;
+document.querySelector('.QNo1 .AnsNo3 .ans-pr').innerHTML=Q1Ans3;
+document.querySelector('.QNo1 .AnsNo4 .ans-pr').innerHTML=Q1Ans4;
+
+
+
+
+
 
 
 
@@ -51,7 +70,7 @@ for (i=0;i<sq.length;i++){
     }
 
 var fin_ans=[];
-const right_ans=["AnsNo0","AnsNo1"]
+const right_ans=["AnsNo3","AnsNo2"]
 
 
 function finish_test(){
@@ -59,12 +78,10 @@ function finish_test(){
     raw_fin_ans=document.querySelectorAll('.chosen');
     for(i=0;i<raw_fin_ans.length;i++){
         raw_fin_ans[i].classList.remove('vis_chosen', 'chosen', 'ans-item');
-        fin_ans.push(raw_fin_ans[i].classList);
-        if (fin_ans[i]==right_ans[i]){score=+1;}
-        else{score=score}
+        fin_ans.push(String(raw_fin_ans[i].classList).split('AnsNo')[1]);
         raw_fin_ans[i].classList.add('ans-item');
     }
-    alert(score);
+    alert(fin_ans);
     score=0;
     choose_6();
 
---
+++ for-newbies/number.js
@@ -0,0 +1,1 @@
+var the_no=1;
