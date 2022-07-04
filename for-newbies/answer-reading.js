a5ee01199982b960dd8ec88097015c8075765278
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2022-07-04 22:36:23 +0800

    Update answer-reading.js

--- for-newbies/answer-reading.js
+++ for-newbies/answer-reading.js
@@ -1,17 +1,23 @@
 const right_ans=[4,3];
 var r_a=[];
 var score=0;
+var final_ans=[];
 
 for(i=0;i<right_ans.length;i++){
     r_a.push((right_ans[i]-1).toString());
 }
 
+for(i=0;i<fin_ans.length;i++){
+    final_ans.push((fin_ans[i]-1).toString());
+}
+
+
 function reading_a(){
     for(i=0;i<r_a.length;i++){
         if(r_a[i]==fin_ans[i]){score+=1}
     }
-    alert('填答：'+fin_ans);
-    alert('正解：'+r_a);
+    alert('填答：'+final_ans);
+    alert('正解：'+right_ans);
     if (score==r_a.length){
         alert('答對了，密碼是「關關雎鳩」')
     }
