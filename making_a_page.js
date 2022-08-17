5449566cfa0e7ac4add8b358fb8accab6a1f7ff1
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2022-08-17 12:39:39 +0800

    Update making_a_page.js

--- making_a_page.js
+++ making_a_page.js
@@ -125,7 +125,16 @@ function getready(area){
     }
 }
 
-fetch('https://vomiter-scp-zh.github.io/bundle_o_bs4/top200.json')
+
+var HEADERS=new Headers();
+HEADERS.append('Pragma','No-Cache');
+HEADERS.append('Cache-Control','No-Cache');
+var INIT={
+    method:'GET',
+    headers:HEADERS
+};
+
+fetch('https://vomiter-scp-zh.github.io/bundle_o_bs4/top200.json',INIT)
 .then(function(response){return response.json();})
 .then(function(tth){
     herearea='#original_area';
